c0570ecc10cdd8547d2fd535a202cbf0c9afe93a
abcLiyew <18378500963@163.com>
2025-06-03 14:23:14 +0800

    feat(model): 新增队伍更新请求和用户信息 VO 类
    - 新增 TeamUpdateRequest 类，用于处理队伍更新请求
    - 新增 TeamUserVo 类，用于封装队伍和用户信息（脱敏）
    - 新增 UserVo 类，用于封装用户信息（脱敏）

---
+++ src/main/java/com/esdllm/catFriend/model/vo/UserVo.java
@@ -0,0 +1,84 @@
+package com.esdllm.catFriend.model.vo;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableLogic;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * 用户包装类（脱敏）
+ */
+@Data
+public class UserVo {
+    /**
+     * 用户id
+     */
+    private Long id;
+
+    /**
+     * 昵称
+     */
+    private String username;
+
+    /**
+     * 登录账户
+     */
+    private String userAccount;
+
+    /**
+     * 用户头像
+     */
+    private String avatarUrl;
+
+    /**
+     * 性别
+     */
+    private Integer gender;
+
+    /**
+     * 电话
+     */
+    private String phone;
+
+    /**
+     * 邮箱
+     */
+    private String email;
+
+    /**
+     * 状态 0-正常
+     */
+    private Integer userStatus;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
+
+    /**
+     * 用户角色 0-普通用户 1-管理员
+     */
+    private Integer userRole;
+
+    /**
+     * 校验编号，用于设置后续校验码
+     */
+    private String inspectionCode;
+
+    /**
+     * 个人简介
+     */
+    private String profile;
+    /**
+     * 标签列表JSON
+     */
+    private String tags;
+}
